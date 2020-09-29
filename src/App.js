a3653a0eb585c4ff06f7e96362e4a2f20c4989d5
Jessé Satlei <jessesatlei@hotmail.com>
2020-09-29 20:55:39 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Projeto Redux</h1>
+      <div className="linha">
+        <Card
+          title="Card 1"
+          red
         >
-          Learn React
-        </a>
-      </header>
+          Teste 1
+        </Card>
+      </div>
+      <div className="linha">
+        <Card
+          title="Card 2"
+          green
+        >
+          Teste 2
+        </Card>
+
+        <Card
+          title="Card 3"
+          blue
+        >
+          Teste 3
+        </Card>
+
+        <Card
+          title="Card 4"
+          purple
+        >
+          Teste 4
+        </Card>
+      </div>
     </div>
   );
 }
