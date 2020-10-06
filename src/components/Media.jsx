e88317f506b938925293456ba5a657116c2c5dab
Jessé Satlei <jessesatlei@hotmail.com>
2020-10-06 20:56:12 -0300

    changes

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 import Card from './Card';
 
 export default props => {
-  const { min, max } = props;
-
 
   return (
     <Card title="Media dos Números" green>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>{(min+max)/2}</strong>
+          <strong>{10}</strong>
         </span>
       </div>
     </Card>
--- src/components/Sorteio.jsx
+++ src/components/Sorteio.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 import Card from './Card';
 
 export default props => {
-  const { min, max } = props;
-  const aleatorio = parseInt(Math.random() * (max - min)) + min;
 
   return (
     <Card title="Sorteio dos Números" purple>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong> { aleatorio } </strong>
+          <strong> { 50 } </strong>
         </span>
       </div>
     </Card>
