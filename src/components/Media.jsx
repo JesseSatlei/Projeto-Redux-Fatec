4ba3346cf16858c1b8b05769710d53c82dc5bb82
Jessé Satlei <jessesatlei@hotmail.com>
2020-10-07 07:39:27 -0300

    changes 06/10

--- src/components/Media.jsx
+++ src/components/Media.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 
 import Card from './Card';
 
-export default props => {
+import { connect } from 'react-redux';
+
+function Media (props) {
+  const { max, min } = props;
 
   return (
     <Card title="Media dos Números" green>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong>{10}</strong>
+          <strong>{(max + min) / 2}</strong>
         </span>
       </div>
     </Card>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Media)
--- src/components/Soma.jsx
+++ src/components/Soma.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 
 import Card from './Card';
 
-export default props => {
+import { connect } from 'react-redux';
+
+function Soma (props) {
+  const { min, max } = props;
 
   return (
     <Card title="Soma dos Números" blue>
       <div>
         <span>
           <span>Resultado: </span>
-          <strong> { 20 } </strong>
+          <strong> { min + max } </strong>
         </span>
       </div>
     </Card>
   );
 }
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Soma)
